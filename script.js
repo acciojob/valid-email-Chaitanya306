b8f07999ca32326d861f8d77b95da909872a1627
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-04-16 12:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	const arr=['@','.']
+	const l=str.length
+  if(!str.includes('@') || !str.includes('.') ||
+	 str.indexOf('@')>str.indexOf('.') || arr.includes(str[0]) ||
+	arr.includes(str[l-1])){
+	  return false
+	}
+	else{ return true }
 }
 
 // Do not change the code below.
